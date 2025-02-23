c031f467503e32ecd49789759d9b811e7578dd5d
Vinith Reddy Banda <116493221+vinithreddybanda@users.noreply.github.com>
2025-02-23 12:15:09 +0530

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,9 +25,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased fade-in`}
       >
-        {children}
+        <div className="glass">
+          {children}
+        </div>
       </body>
     </html>
   );
