06b48524691a155306b4b468464098f238877d73
vinithreddybanda <vinithreddy08.123@gmail.com>
2025-03-27 01:04:18 +0530

    new

--- src/app/page.js
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <main className="flex min-h-screen items-center justify-center bg-gray-100">
-      <h1 className="text-4xl font-bold text-blue-600">Welcome to Classlite 🚀</h1>
-    </main>
-  );
-}
